24253be909abf97eb671f72a548c572ccb31adb3
Martin <stamnitram@gmail.com>
2022-05-20 20:20:10 +0200

    cart-slice finnished

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -25,9 +25,18 @@ const cartSlice = createSlice({
         existingItem.totalPrice += newItem.totalPrice;
       }
     },
-    removeItemToCart(state) {},
+    removeItemToCart(state, action) {
+      const id = action.payload;
+      const existingItem = state.items.find((item) => item.id === id);
+      if (existingItem.quantity === 1) {
+        state.items = state.items.filter((item) => item.id !== id);
+      } else {
+        existingItem.quantity--;
+        existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
+      }
+    },
   },
 });
 
+export const cartActions = cartSlice.actions;
 export default cartSlice.reducer;
-export const uiActions = cartSlice.actions;
