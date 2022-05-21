45a4c9e3c05f726890c9d6f96718a32a8a54cbf5
Martin <stamnitram@gmail.com>
2022-05-21 23:22:48 +0200

    connected with useefect but with not configurate firebase bug

--- src/App.js
+++ src/App.js
@@ -2,10 +2,22 @@ import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 
+import { useEffect } from "react";
 import { useSelector } from "react-redux";
 
 function App() {
+  const cart = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    fetch("https://redux-cart-f8db2-default-rtdb.firebaseio.com/", {
+      method: "PUT",
+      body: JSON.stringify(cart),
+    });
+  }, [cart]);
+
   const isCartVisible = useSelector((data) => data.ui.isCartVisible);
+
+  useSelector((state) => state.cart);
   return (
     <Layout>
       {isCartVisible ? <Cart /> : null}
---
+++ src/store/firebase.config.js
@@ -0,0 +1,21 @@
+// import { initializeApp } from "firebase/app";
+// import { getFirestore } from "@firebase/firestore";
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAdRUaGJtioclE5Pr9LovGm2NTTjW8Bu7M",
+
+//   authDomain: "redux-cart-f8db2.firebaseapp.com",
+
+//   projectId: "redux-cart-f8db2",
+
+//   storageBucket: "redux-cart-f8db2.appspot.com",
+
+//   messagingSenderId: "451185728372",
+
+//   appId: "1:451185728372:web:ea7e785c2d776fb9a42a21",
+// };
+
+// // Initialize Firebase and export
+// const app = initializeApp(firebaseConfig);
+// const db = getFirestore(app);
+// export { db };
