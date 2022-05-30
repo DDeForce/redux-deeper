897c52088faf8d2babd26e76d1db470a24f69385
Martin <stamnitram@gmail.com>
2022-05-31 01:38:20 +0200

    added firebase

--- src/components/Layout/MainHeader.js
+++ src/components/Layout/MainHeader.js
@@ -1,7 +1,15 @@
-import CartButton from '../Cart/CartButton';
-import classes from './MainHeader.module.css';
+import CartButton from "../Cart/CartButton";
+import classes from "./MainHeader.module.css";
+// import { collection, addDoc } from "firebase/firestore";
+// import { db } from "../../store/firebase.config";
 
 const MainHeader = (props) => {
+  // const recipesCollectionRef = collection(db, "test");
+
+  // const testFBaase = () => {
+  //   addDoc(recipesCollectionRef, { test1: "proslo2" });
+  // };
+
   return (
     <header className={classes.header}>
       <h1>ReduxCart</h1>
@@ -9,6 +17,7 @@ const MainHeader = (props) => {
         <ul>
           <li>
             <CartButton />
+            {/* <button onClick={(e) => testFBaase(e)}>Test firebase</button> */}
           </li>
         </ul>
       </nav>
